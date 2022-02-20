cd521af8bc04b8d46d73509c30f8c695cd475652
natsumisawa <natsumi.sawa@bizreach.co.jp>
2022-02-20 17:49:41 +0900

    ✨lesson1

--- commands/run.py
+++ commands/run.py
@@ -1,2 +1,24 @@
-def exec_run():
+import linux
+import os
+import click
+from typing import List
+
+def hello(option: dict):
+    command = option["commands"]
+
+    print(f'this is child process. ${os.getpid()}') # same to parent pid number
+
+    os.execvp(command[0], command) # exec command program from child process
+
+def exec_run(commands: List[str]):
     print(f'run command called!')
+    flags = 0
+    option = {"commands": commands} # add other parameter after
+
+    pid = linux.clone(hello, flags, (option,)) # system call
+    print(pid) # print parent pid number
+    os.waitpid(pid, 0)
+
+    # ex) exec -> run echo hello
+    # run command called!
+    # hello
